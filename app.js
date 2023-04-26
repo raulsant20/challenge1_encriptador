5f717748b0a74ac076848ddad1a7d8b363e4d78e
Raul Santa Cruz <raulsantacruz0@gmail.com>
2023-04-25 22:06:50 -0500

    create decripting and copying functions

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const originalText = document.getElementsByName("originalText")[0];
 const encryptedPlace = document.getElementsByClassName("encrypted")[0];
-console.log(originalText.value);
-console.log(encryptedPlace.value);
+// console.log(originalText.value);
+// console.log(encryptedPlace.value);
 
 const encryptButton = document.getElementById("encrypt");
 const decryptButton = document.getElementById("decrypt");
 
-console.log(encryptButton.textContent, decryptButton.textContent);
+// console.log(encryptButton.textContent, decryptButton.textContent);
 
 const encrypting = () => {
   let encryptedText = originalText.value.toLowerCase().split("");
@@ -27,9 +27,41 @@ const encrypting = () => {
     }
   });
 
-  console.log(encryptedText);
-  encryptedPlace.innerHTML = encryptedText.join("");
-  originalText.value = "";
+  // console.log(encryptedText);
+  if (encryptedText.length != 0) {
+    let result = document.createElement("div");
+    result.id = "result";
+
+    result.innerHTML = encryptedText.join("");
+    console.log(result.textContent);
+    encryptedPlace.appendChild(result);
+    originalText.value = "";
+
+    let copyButton = document.createElement("button");
+    copyButton.id = "copy";
+    copyButton.innerText = "Copiar";
+    copyButton.onclick = function () {
+      navigator.clipboard.writeText(result.textContent);
+    };
+    encryptedPlace.appendChild(copyButton);
+  }
+};
+
+const decrypting = () => {
+  let decryptedText = originalText.value
+    .toLowerCase()
+    .replaceAll("ai", "a")
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+  // console.log(decryptedText);
+  if (decryptedText.length != 0) {
+    encryptedPlace.innerHTML = decryptedText;
+    originalText.value = "";
+  }
 };
 
 encryptButton.addEventListener("click", encrypting);
+
+decryptButton.addEventListener("click", decrypting);
